12d1b3c4186661a5219ebc577108ddedba2a5942
abriku <oaoandrew@gmail.com>
2019-10-09 19:25:02 +0300

    feat: add some comment

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   module.exports = function check(str, bracketsConfig) {
-    //let conf = bracketsConfig.join();
+
     let result = [];
     
     for (let i=0; i < str.length; i++) {
       for (let j=0; j < bracketsConfig.length; j++) {
-          
+          /* push but not the same */
           if (str[i] == bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1]) {
               result.push(bracketsConfig[j][0]);
           }
-  
+          /* the same */
           else if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
-            if (result[result.length-1] == bracketsConfig[j][0]) {
+            if (result[result.length-1] == bracketsConfig[j][0]) {    // last symbol check
               result.pop();
             }
             else {
               result.push(bracketsConfig[j][0]);
             }
           }
-  
+          /* pop not the same */
           else if (str[i] == bracketsConfig[j][1] && str[i]!== bracketsConfig[j][0]) {
             if (result[result.length-1] == bracketsConfig[j][0]) {
               result.pop();
