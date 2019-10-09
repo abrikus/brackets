67c73c6a8c1227d9f3b0dc639aa48475ecf2a0fc
abriku <oaoandrew@gmail.com>
2019-10-09 17:00:31 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  module.exports = function check(str, bracketsConfig) {
+    //let conf = bracketsConfig.join();
+    let result = [];
+    
+    for (let i=0; i < str.length; i++) {
+      for (let j=0; j < bracketsConfig.length; j++) {
+          
+          if (str[i] == bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1]) {
+              result.push(bracketsConfig[j][0]);
+          }
+  
+          else if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
+            if (result[result.length-1] == bracketsConfig[j][0]) {
+              result.pop();
+            }
+            else {
+              result.push(bracketsConfig[j][0]);
+            }
+          }
+  
+          else if (str[i] == bracketsConfig[j][1] && str[i]!== bracketsConfig[j][0]) {
+            if (result[result.length-1] == bracketsConfig[j][0]) {
+              result.pop();
+              }
+              else {
+                result.push(str[i]);
+              }
+          }
+      }
+    }
+    return result.length == 0;
+  }
 }
